8350c200797ec7aad84aabf2bb811a2f0c113f79
shiv <shivakumarsuryan@gmail.com>
2019-10-04 11:01:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/Junit5sampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5sampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5sampleApplication.class, args);
+	}
+
+}
