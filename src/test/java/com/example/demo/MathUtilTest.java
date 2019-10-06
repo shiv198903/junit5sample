e0f2400fdaa8a38e67a7fbc4ed03abfdfcf7c5b9
shiv <shivakumarsuryan@gmail.com>
2019-10-06 08:35:32 +0530

    Added @TestInstance annotation.

--- src/test/java/com/example/demo/MathUtilTest.java
+++ src/test/java/com/example/demo/MathUtilTest.java
@@ -5,7 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+/*Makes junit to initialize the class only once in the 
+test run. This can be used in cases where we want 
+to share the member variables between different tests.
+Although the sequeunce in which the tests are executed 
+is not constant.*/
+@TestInstance(Lifecycle.PER_CLASS)
 class MathUtilTest {
 
 	private MathUtil mathUtil;
