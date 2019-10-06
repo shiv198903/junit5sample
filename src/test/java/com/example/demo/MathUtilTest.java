7ae73947b981ea1a661093cc11de0880b43c2420
shiv <shivakumarsuryan@gmail.com>
2019-10-06 07:38:59 +0530

    Test Exception thrown by a method.
    Test the exception type that is thrown by a method in case of exception
    scenario.

--- src/test/java/com/example/demo/MathUtilTest.java
+++ src/test/java/com/example/demo/MathUtilTest.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class MathUtilTest {
-	
+
 	private MathUtil mathUtil = new MathUtil();
 
 	@Test
 	void testAdd() {
 		assertEquals(2, mathUtil.add(1, 1), "Should return correct sum.");
+	}
+
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtil.divide(1, 0),
+				"Divide by zero should throw an Arithmatic exception.");
 	}
 
 }
