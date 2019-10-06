8fdf80fd321c34b59c0d2a48f5d7eaade35bb247
shiv <shivakumarsuryan@gmail.com>
2019-10-06 08:26:13 +0530

    Used @BeforeEach to initialize MathUtil.
    Removed explicit initialization of MathUtil and used JUnit hook instead.

--- src/test/java/com/example/demo/MathUtilTest.java
+++ src/test/java/com/example/demo/MathUtilTest.java
@@ -1,20 +1,29 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilTest {
 
+	private MathUtil mathUtil;
+
+	@BeforeEach
+	void initMathUtil() {
+		mathUtil = new MathUtil();
+	}
+
 	@Test
 	void testAdd() {
-		MathUtil mathUtil = new MathUtil();
+		mathUtil = new MathUtil();
 		assertEquals(2, mathUtil.add(1, 1), "Should return correct sum.");
 	}
 
 	@Test
 	void testDivide() {
-		MathUtil mathUtil = new MathUtil();
+		mathUtil = new MathUtil();
 		assertThrows(ArithmeticException.class, () -> mathUtil.divide(1, 0),
 				"Divide by zero should throw an Arithmatic exception.");
 	}
