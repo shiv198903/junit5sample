f472ac44ecfc1805d10f3b8187020e07b4fdd12e
shiv <shivakumarsuryan@gmail.com>
2019-10-06 08:04:22 +0530

    Removed common instance of MathUtil.
    Removed the common instance of MathUtil that is being shared by all the
    methods to execute tests. Ideally each test should have a clean object
    to run tests on.

--- src/test/java/com/example/demo/MathUtilTest.java
+++ src/test/java/com/example/demo/MathUtilTest.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class MathUtilTest {
 
-	private MathUtil mathUtil = new MathUtil();
-
 	@Test
 	void testAdd() {
+		MathUtil mathUtil = new MathUtil();
 		assertEquals(2, mathUtil.add(1, 1), "Should return correct sum.");
 	}
 
 	@Test
 	void testDivide() {
+		MathUtil mathUtil = new MathUtil();
 		assertThrows(ArithmeticException.class, () -> mathUtil.divide(1, 0),
 				"Divide by zero should throw an Arithmatic exception.");
 	}
